8c12979b90ab6bd4e2165e7cc37917fe6e634051
SimDamDev <simon.damico@outlook.com>
2023-03-09 23:57:22 +0100

    test app.js modification

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Frigo Flow
-      </header>
-      <h1>ceci est un test</h1>
-      <h2>vu que ca marchait pas, ceci est un autre test</h2>
-    </div>
+      <div className="App">
+          <header className="App-header">Frigo Flow</header>
+          <body>
+              <h1>ceci est un test</h1>
+              <h2>vu que ca marchait pas, ceci est un autre test</h2>
+          </body>
+      </div>
   );
 }
 
