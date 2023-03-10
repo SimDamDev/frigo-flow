daae3ae5777339e93678f76ef8884e924af07531
SimDamDev <simon.damico@outlook.com>
2023-03-10 14:39:13 +0100

    add menu item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import HorizontalMenu from './components/HorizontalMenu';
 
 function App() {
   return (
       <div className="App">
           <header className="App-header">Frigo Flow</header>
           <body>
-              <h1>ceci est un test</h1>
-              <h2>vu que ca marchait pas, ceci est un autre test</h2>
+              <HorizontalMenu />
           </body>
       </div>
   );
---
+++ src/components/HorizontalMenu.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Component } from "react";
+import { Menu } from "antd";
+
+const HorizontalMenu = () => {
+    return (
+        <Menu mode="horizontal">
+            <Menu.Item key="home">Accueil</Menu.Item>
+            <Menu.Item key="database">Database</Menu.Item>
+            <Menu.Item key="add ingredient">Ajouter ingredients</Menu.Item>
+            <Menu.Item key="add recipes">Ajouter recettes</Menu.Item>
+            <Menu.Item key="login" style={{ marginLeft: "auto" }}>
+                Login
+            </Menu.Item>
+        </Menu>
+    );
+};
+
+export default HorizontalMenu;
